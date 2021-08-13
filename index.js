0933fb8e67dd9d9a106201f61f85a502f28cde14
Dan <DanielMooreRealPerson@gmail.com>
2021-08-13 15:18:31 -0400

    commit tree c3f5678b1155540208a2fc5f19561f6f78364840

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions here
+const map = (arr, func) => {
+    const emptyArr = []
+    for (let element of arr) {
+        emptyArr.push(func(element))
+    }
+    return emptyArr
+}
+
+const reduce = (arr, func, startPoint=0) => {
+    if (!!startPoint == false) {
+        startPoint = arr.shift()
+    }
+    return arr.reduce(func, startPoint)
+}
